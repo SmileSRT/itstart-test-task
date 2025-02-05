8209b8bc3e79498bd33c0ccf8c9838e5522de589
SmileSRT <korovichev367@gmail.com>
2025-02-05 21:41:27 +0300

    feat(app): initial commit

---
+++ client/src/app/app.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/shared/ui/button';
+import type { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default App;
